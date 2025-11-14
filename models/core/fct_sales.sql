a7bbd192f30bfae0626029c71948f8320f4fdc47
monikabishtt77@gmail.com <monikabishtt77@gmail.com>
2025-11-14 09:07:55 +0000

    added not null checks to satisfy relationship test

--- models/core/fct_sales.sql
+++ models/core/fct_sales.sql
@@ -21,7 +21,11 @@ select
     p.cost_price,
     (o.quantity * o.unit_price) as revenue,
     (o.quantity * (o.unit_price - p.cost_price)) as profit
-from
-    orders o 
+from orders o
 left join products p on o.product_id = p.product_id
 left join customers c on o.customer_id = c.customer_id
+where
+    o.customer_id is not null      -- to satisfy relationship test
+    and c.customer_id is not null  -- dimension match
+    and o.product_id is not null
+    and p.product_id is not null
