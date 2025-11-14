58a936450919b669a112fa6f945036cc3857d3ae
monikabishtt77@gmail.com <monikabishtt77@gmail.com>
2025-11-14 09:05:34 +0000

    added not null check for invoiceno

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,22 +1,27 @@
 with orders_source as (
-select
-    *
-from
-    {{ source('ecommerce_analytics_dbt', 'orders') }}
-)
-, orders_updated as (
-select
-    invoiceno as order_id,
-    stockcode as product_id,
-    description as product_name,
-    quantity,
-    try_to_timestamp(invoicedate, 'MM/DD/YYYY HH24:MI') as order_date,
-    unitprice as unit_price,
-    (unitprice * quantity) as total_value,
-    split_part(to_varchar(customerid), '.', 1) as customer_id,
-    trim(country) as country
-from
-    orders_source
+    select *
+    from raw.ecommerce_analytics_dbt.orders
+),
+
+cleaned as (
+    select
+        invoiceno as order_id,
+        stockcode as product_id,
+        description as product_name,
+        quantity,
+
+        try_to_timestamp(invoicedate) as order_date,
+
+        unitprice as unit_price,
+        (unitprice * quantity) as total_value,
+
+        -- Consistent cleaning
+        split_part(to_varchar(customerid), '.', 1) as customer_id,
+
+        trim(country) as country
+    from orders_source
+    where invoiceno is not null        -- only remove fully broken rows
 )
 
-select * from orders_updated
+select *
+from cleaned
